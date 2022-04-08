ea63302a2a6ddf24435489598bfabfe3d1038e54
dk [OFLINE] <98028377+vloggerdeven@users.noreply.github.com>
2022-04-08 20:40:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/vloggerdeven/vellachilaano.git /vellachilaano
+  git clone https://github.com/vloggerdeven/Perfect-Ok.git /Perfect-Ok
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /vellachilaano
+  git clone $UPSTREAM_REPO /Perfect-Ok
 fi
-cd /vellachilaano
+cd /Perfect-Ok
 pip3 install -U -r requirements.txt
 echo "Starting 𝘋𝘒_𝘉𝘖𝘛𝘹...."
 python3 bot.py
